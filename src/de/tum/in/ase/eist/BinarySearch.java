357a5cc6b2eb76baa3c5b5f75eaa8a211c6eaa0d
moritzslz <schultzmoritz@gmail.com>
2023-06-29 01:11:39 +0200

    BinarySearch performSearch

--- src/de/tum/in/ase/eist/BinarySearch.java
+++ src/de/tum/in/ase/eist/BinarySearch.java
@@ -5,26 +5,26 @@ import java.util.List;
 public class BinarySearch {
 
     public int  performSearch(List<Chapter> chapters, String name) {
-        chapters.stream().sorted();
         int lowerBound = 0;
         int upperBound = chapters.size() - 1;
         int mid = (lowerBound + upperBound) / 2;
         int comperator = 0;
 
-        if (chapters.get(lowerBound) == chapters.get(upperBound))
+        if (chapters.get(lowerBound) == chapters.get(upperBound)) {
             return chapters.get(mid).getPageNumber();
+        }
 
         while (lowerBound < upperBound) {
             mid = (lowerBound + upperBound) / 2;
             comperator = chapters.get(mid).getName().compareTo(name);
-            if (comperator > 0)
+            if (comperator > 0) {
                 lowerBound = mid + 1;
-            else if (comperator < 0)
+            } else if (comperator < 0) {
                 upperBound = mid - 1;
-            else if (chapters.get(mid).getName().equals(name))
+            } else {
                 return chapters.get(mid).getPageNumber();
+            }
         }
-
         return -1;
     }
 }
