c755ace0de5c1ffbff5ba7c55f62be2a0441cb81
moritzslz <schultzmoritz@gmail.com>
2023-06-29 01:05:45 +0200

    BinarySearch performSearch

--- src/de/tum/in/ase/eist/BinarySearch.java
+++ src/de/tum/in/ase/eist/BinarySearch.java
@@ -1,12 +1,14 @@
 package de.tum.in.ase.eist;
 
+import java.util.Comparator;
 import java.util.List;
 
 public class BinarySearch {
 
     public int  performSearch(List<Chapter> chapters, String name) {
+        chapters.stream().sorted();
         int lowerBound = 0;
-        int upperBound = chapters.size()- 1;
+        int upperBound = chapters.size() - 1;
         int mid = (lowerBound + upperBound) / 2;
         int comperator = 0;
 
@@ -14,9 +16,8 @@ public class BinarySearch {
             return chapters.get(mid).getPageNumber();
 
         while (lowerBound < upperBound) {
-            comperator = 0;
             mid = (lowerBound + upperBound) / 2;
-            comperator = chapters.get(mid).getName().compareTo(name);
+            comperator = name.compareTo(chapters.get(mid).getName());
             if (comperator < 0)
                 lowerBound = mid + 1;
             else if (comperator > 0)
@@ -24,12 +25,7 @@ public class BinarySearch {
             else if (chapters.get(mid).getName().equals(name))
                 return chapters.get(mid).getPageNumber();
         }
-        int comperator2 = chapters.get(0).getName().compareTo(name);
-        int comperator3 = chapters.get(chapters.size() - 1).getName().compareTo(name);
-        if (comperator2 < 0)
-            return -1;
-        else if (comperator3 > 0)
-            return -1;
+
         return -1;
     }
 }
